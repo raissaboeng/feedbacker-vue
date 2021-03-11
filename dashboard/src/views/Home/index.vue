444e94b3aced88fdd2f468d2df37f2d0de778f54
raissaboeng <raissa.boeng@gmail.com>
2021-03-11 17:54:14 -0300

    add page home

--- dashboard/src/views/Home/index.vue
+++ dashboard/src/views/Home/index.vue
@@ -1,7 +1,37 @@
 <template>
-  <h1 class="font-black text-brand-main">home</h1>
+  <custom-header @create-account="handleAccountCreate" @login="handleLogin" />
+  <contact />
+  <div class="flex justify-center py-10 bg-brand-gray">
+    <p class="font-medium text-center text-gray-800">feedbacker © 2021</p>
+  </div>
 </template>
 
 <script>
-export default {};
+import { onMounted } from "vue";
+import { useRouter } from "vue-router";
+import CustomHeader from "./CustomHeader.vue";
+import Contact from "./Contact.vue";
+
+export default {
+  components: { CustomHeader, Contact },
+  setup() {
+    const router = useRouter();
+
+    onMounted(() => {
+      const token = window.localStorage.getItem("token");
+      if (token) {
+        router.push({ name: "Feedbacks" });
+      }
+    });
+
+    function handleLogin() {}
+
+    function handleAccountCreate() {}
+
+    return {
+      handleLogin,
+      handleAccountCreate,
+    };
+  },
+};
 </script>
