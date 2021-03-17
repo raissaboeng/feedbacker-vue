5215b8fd4c5979d98421b8159daec4c6b1a293c9
raissaboeng <raissa.boeng@gmail.com>
2021-03-17 20:11:29 -0300

    add comunication backend login and service auth

--- dashboard/src/components/ModalLogin/index.vue
+++ dashboard/src/components/ModalLogin/index.vue
@@ -62,13 +62,18 @@
 
 <script>
 import { reactive } from 'vue';
+import { useRouter } from 'vue-router';
 import { useField } from 'vee-validate';
+import { useToast } from 'vue-toastification';
 import useModal from '../../hooks/useModal';
 import { validateEmptyAndLegnth3, validateEmptyAndEmail } from '../../utils/validators';
+import services from '../../services';
 
 export default {
   setup () {
+    const router = useRouter();
     const modal = useModal();
+    const toast = useToast();
 
     const {
       value: emailValue,
@@ -93,8 +98,38 @@ export default {
       }
     });
 
-    function handleSubmit () {
+    async function handleSubmit () {
+      try {
+        toast.clear();
+        state.isLoading = true;
+        const { data, errors } = await services.auth.login({
+          email: state.email.value,
+          password: state.password.value
+        });
 
+        if (!errors) {
+          window.localStorage.setItem('token', data.token);
+          router.push({ name: 'Feedbacks' });
+          state.isLoading = false;
+          modal.close();
+          return;
+        }
+        if (errors.status === 404) {
+          toast.error('E-mail não encontrado');
+        }
+        if (errors.status === 401) {
+          toast.error('E-mail/senha inválidos');
+        }
+        if (errors.status === 400) {
+          toast.error('Ocorreu um erro ao fazer login');
+        }
+
+        state.isLoading = false;
+      } catch (error) {
+        state.isLoading = false;
+        state.hasErrors = !!error;
+        toast.error('Ocorreu um erro ao fazer login');
+      }
     }
 
     return {
---
+++ dashboard/src/services/auth.js
@@ -0,0 +1,20 @@
+export default httpClient => ({
+  login: async ({ email, password }) => {
+    const response = await httpClient.post('/auth/login', {
+      email, password
+    });
+    let errors = null;
+
+    if (!response.data) {
+      errors = {
+        status: response.request.status,
+        statusText: response.request.statusText
+      };
+    }
+
+    return {
+      data: response.data,
+      errors
+    };
+  }
+});
