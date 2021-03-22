096f8e11244eb7015d25b0e21e37dd399f256f78
raissaboeng <raissa.boeng@gmail.com>
2021-03-22 19:12:52 -0300

    create page credentials and header menu with navegation and logout

---
+++ dashboard/src/views/Credentials/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="flex justify-center w-full h-28 bg-brand-main">
+    <header-logged />
+  </div>
+  <div class="flex flex-col items-center justify-center h-64 bg-brand-gray">
+    <h1 class="text-4xl font-black text-center text-gray-800">
+      Credenciais
+    </h1>
+    <p class="text-lg text-center text-gray-800 font-regular">
+      Guia de instalação e geração de suas credenciais
+    </p>
+  </div>
+</template>
+
+<script>
+import HeaderLogged from '../../components/HeaderLogged';
+export default {
+  components: { HeaderLogged }
+};
+</script>
