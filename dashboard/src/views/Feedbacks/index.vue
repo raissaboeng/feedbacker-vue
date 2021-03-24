3391419168d634bd734e5b2f937d5b25b689aa74
raissaboeng <raissa.boeng@gmail.com>
2021-03-24 18:02:22 -0300

    add header and primary container in page Feedbacks and create services feedbacks

--- dashboard/src/views/Feedbacks/index.vue
+++ dashboard/src/views/Feedbacks/index.vue
@@ -1,12 +1,21 @@
 <template>
-  <h1>Feedbacks</h1>
+  <div class="flex justify-center w-full h-28 bg-brand-main">
+    <header-logged />
+  </div>
+
+  <div class="flex flex-col items-center justify-center h-64 bg-brand-gray">
+    <h1 class="text-4xl font-black text-center text-gray-800">
+      Feedbacks
+    </h1>
+    <p class="text-lg text-center text-gray-800 font-regular">
+      Detalhes de todos os feedbacks recebidos.
+    </p>
+  </div>
 </template>
 
 <script>
+import HeaderLogged from '../../components/HeaderLogged';
 export default {
-
+  components: { HeaderLogged }
 };
 </script>
-
-<style>
-</style>
