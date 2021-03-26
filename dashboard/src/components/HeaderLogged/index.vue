f25657509d2b355fafb96aec6d6c2042aec193db
raissaboeng <raissa.boeng@gmail.com>
2021-03-26 10:54:21 -0300

    add test with vue-test-utils in component HeaderLogged using store mock

--- dashboard/src/components/HeaderLogged/index.vue
+++ dashboard/src/components/HeaderLogged/index.vue
@@ -24,6 +24,7 @@
           Feedbacks
         </li>
         <li
+          id="logout-button"
           @click="handleLogout"
           class="px-6 py-2 font-bold bg-white rounded-full cursor-pointer text-brand-main focus:outline-none"
         >
